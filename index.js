6a31231d810efa885059745a693478dadc8ad525
Matthammond96 <ultramatt96@hotmail.co.uk>
2022-03-17 15:13:45 +0000

    Merged sumRow and Row Loop

--- index.js
+++ index.js
@@ -8,23 +8,20 @@ module.exports = function (inputShape) {
 
   for (let i = 1; i < y + 1; i++) {
     let row = []
+    let sumRow = []
     
-      for (let j = 1; j < x + 1; j++) {
-        if (j === x) {
-          const sum = row.reduce((prev, cur) => prev + cur)
-          row.push(sum)
-        } else {
-          row.push(j)
-        }
+    for (let j = 1; j < x + 1; j++) {
+      if (j === x) {
+        const sum = row.reduce((prev, cur) => prev + cur)
+        row.push(sum)
+        sumRow.push(sum * (y - 1))
+      } else {
+        row.push(j)
+        sumRow.push(j * (y - 1))
       }
+    }
 
     if (i === y) {
-      const sumRow = []
-
-      for (const [index, value] of result[0].entries()) {
-        sumRow.push(value * (y - 1))
-      }
-
       result.push(sumRow)
     } else {
       result.push(row)
